ee7ee006dbf0630ba0906a9705daff1733027be8
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 23:00:33 +0800

    Added ListCell to display data

---
+++ SimpleMovieSearch/SimpleMovieSearch/Views/MovieListCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListCell.swift
+//  SimpleMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 26/2/25.
+//
+
+import SwiftUI
+
+struct MovieListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListCell()
+}
