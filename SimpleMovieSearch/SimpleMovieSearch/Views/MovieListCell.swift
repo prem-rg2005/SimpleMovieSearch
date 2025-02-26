6b0d330444722187d34be55c151cb4077fabf6ff
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 23:11:18 +0800

    MovieListView with dummy data

--- SimpleMovieSearch/SimpleMovieSearch/Views/MovieListCell.swift
+++ SimpleMovieSearch/SimpleMovieSearch/Views/MovieListCell.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct MovieListCell: View {
+    @State var movie: Movie = .init(title: "Harry Potter Part 7 and just a very long title", year: "2011", id: "tt1201607", type: "movie", poster: "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_SX300.jpg")
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            AsyncImage(url: URL(string: movie.poster)) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else if phase.error != nil {
+                    Text("No image available")
+                } else {
+                    Image.sysImage.placeholderImg
+                }
+            }
+            .frame(width: 100)
+            .cornerRadius(10)
+            
+            VStackLayout(alignment: .leading) {
+                HStack {
+                    Text(movie.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                    Spacer()
+                }
+                Text("Year: \(movie.year)")
+                    .font(.subheadline)
+                    .foregroundColor(Color.themeColor.primaryText)
+            }
+            .padding(.leading, 8)
+        }
+        .padding()
+        .background(Color.themeColor.cellBg)
+        .cornerRadius(10)
+        .padding(.horizontal)
     }
 }
 
