fece70af94df00ccae5446f681f8977ddf8273c4
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 23:13:32 +0800

    Added ListView for movies

---
+++ SimpleMovieSearch/SimpleMovieSearch/Views/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  SimpleMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 26/2/25.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
