ed069d8b071f12beae15358e55ffb46d2f11d246
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 23:37:47 +0800

    Added viewModel logic to ListView

--- SimpleMovieSearch/SimpleMovieSearch/Views/MovieListView.swift
+++ SimpleMovieSearch/SimpleMovieSearch/Views/MovieListView.swift
@@ -16,6 +16,32 @@ struct MovieListView: View {
             VStack {
                 SearchBarView(searchText: $searchQuery, onSearch: searchMovies)
                 
+                if viewModel.isLoading && viewModel.movies.isEmpty {
+                    ProgressView("Loading...")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .frame(maxHeight: .infinity)
+                } else if let errorMessage = viewModel.errorMessage {
+                    Text("Error: \(errorMessage)")
+                        .foregroundColor(.red)
+                        .frame(maxHeight: .infinity)
+                } else {
+                    ForEach(viewModel.movies) { movie in
+                        MovieListCell(movie: movie)
+                            .listRowSeparator(.hidden)
+                    }
+                    
+                    if viewModel.shouldLoadMoreMovies() {
+                        HStack {
+                            Spacer()
+                            Text("Loading more movies.....")
+                                .padding()
+                            Spacer()
+                        }
+                        .onAppear {
+                            viewModel.fetchMovies()
+                        }
+                    }
+                }
             }
             .navigationTitle("Movie List")
         }
