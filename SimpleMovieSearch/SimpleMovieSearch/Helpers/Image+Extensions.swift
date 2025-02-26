6845742efdd4a2bd5d7d2afacbc52740d30276d4
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 22:30:28 +0800

    Initial file for images

---
+++ SimpleMovieSearch/SimpleMovieSearch/Helpers/Image+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Image+Extensions.swift
+//  SimpleMovieSearch
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 26/2/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    static let sysImage = SystemImageNames()
+}
+
+struct SystemImageNames {
+}
