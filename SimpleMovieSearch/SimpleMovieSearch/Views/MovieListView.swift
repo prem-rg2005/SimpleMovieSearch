7b9eaae9b6f2fac283a2f662d579caa8237c2fee
Prem Shankar <prem.rg2005@gmail.com>
2025-02-26 23:42:42 +0800

    Embed using list and removed separator lines

--- SimpleMovieSearch/SimpleMovieSearch/Views/MovieListView.swift
+++ SimpleMovieSearch/SimpleMovieSearch/Views/MovieListView.swift
@@ -25,27 +25,27 @@ struct MovieListView: View {
                         .foregroundColor(.red)
                         .frame(maxHeight: .infinity)
                 } else {
-                    ForEach(viewModel.movies) { movie in
-                        MovieListCell(movie: movie)
-                            .listRowSeparator(.hidden)
-                    }
-                    
-                    if viewModel.shouldLoadMoreMovies() {
-                        HStack {
-                            Spacer()
-                            Text("Loading more movies.....")
-                                .padding()
-                            Spacer()
+                    List {
+                        ForEach(viewModel.movies) { movie in
+                            MovieListCell(movie: movie)
                         }
-                        .onAppear {
-                            viewModel.fetchMovies()
+                        if viewModel.shouldLoadMoreMovies() {
+                            HStack {
+                                Spacer()
+                                Text("Loading more movies.....")
+                                    .padding()
+                                Spacer()
+                            }
+                            .onAppear {
+                                viewModel.fetchMovies()
+                            }
                         }
                     }
+                    .listStyle(PlainListStyle())
                 }
             }
             .navigationTitle("Movie List")
         }
-        
     }
     
     private func searchMovies() {
